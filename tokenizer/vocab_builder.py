1472f3ad84163001b5f16a71fed57bda222d94a0
Ali Huzaifa <designerhuzaifa12@gmail.com>
2026-02-21 04:34:28 +0500

    updated vocab builder

--- tokenizer/vocab_builder.py
+++ tokenizer/vocab_builder.py
@@ -7,3 +7,24 @@ def build_vocab(corpus, vocab_size=500):
     linguistic units + entropy-based pruning.
     """
     vocab = set()
+
+    # Generate substrings
+    for sentence in corpus:
+        for i in range(len(sentence)):
+            for j in range(i + 1, len(sentence) + 1):
+                vocab.add(sentence[i:j])
+
+    probs = initialize_probabilities(vocab)
+    probs = em_algorithm(corpus, vocab, probs)
+
+    # Prune vocabulary
+    while len(vocab) > vocab_size:
+        scores = {
+            v: branching_entropy(v, corpus) * probs[v]
+            for v in vocab
+        }
+        remove_count = max(1, len(vocab) // 5)
+        for token in sorted(scores, key=scores.get)[:remove_count]:
+            vocab.remove(token)
+
+    return list(vocab), probs
