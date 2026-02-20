aecf3a5dd2bb34a2fcf71cd9effd481ceee5eaa7
Ali Huzaifa <designerhuzaifa12@gmail.com>
2026-02-20 13:49:09 +0500

    updated thunder tok

--- tokenizer/thunder_tok.py
+++ tokenizer/thunder_tok.py
@@ -5,4 +5,14 @@ class ThunderTok:
     def __init__(self, vocab, probs):
         self.unigram = UnigramTokenizer(vocab, probs)
 
+    def tokenize(self, text):
+        """
+        Full Thunder-Tok pipeline.
+        """
+        segments = pretokenize(text)
+        tokens = []
 
+        for seg in segments:
+            tokens.extend(self.unigram.tokenize(seg))
+
+        return tokens
